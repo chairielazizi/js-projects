6a51aa5f1412719da03fdfea16dde9d9901a44e4
chairielazizi <azizihr2000@gmail.com>
2020-08-08 12:59:59 +0800

    finished question project

--- Question/app.js
+++ Question/app.js
@@ -1,2 +1,33 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(que){
+    // console.log(que);
+    const btn = que.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click',function(){
+        questions.forEach(function(param){
+            console.log(param);
+            if(param !== que){
+                param.classList.remove('show-text');
+            }
+        });
+
+        que.classList.toggle('show-text');
+    });
+});
+
+
+
 // traversing the dom
+
+// const buttons = document.querySelectorAll('.question-btn');
+
+// buttons.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         // console.log(e.currentTarget.parentElement.parentElement);
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
