603e3ee5276c19793dd18c6638ab3f7b83368960
chairielazizi <azizihr2000@gmail.com>
2021-02-13 21:49:32 +0800

    add comment to current target of event listener which is the class

--- Counter/app.js
+++ Counter/app.js
@@ -1,5 +1,5 @@
 // seti initial count
-let count =0;
+let count = 0;
 
 //select values and buttons
 // querySelector can made into arraylike and use forEach()
@@ -9,27 +9,24 @@ const buttons = document.querySelectorAll(".btn");
 console.log(buttons);
 
 buttons.forEach(function (item) {
-    console.log(item);
-    item.addEventListener('click',function(e){
-        const styles = (e.currentTarget.classList);
-        if(styles.contains('decrease')){
-            count--;
-        }
-        else if(styles.contains('increase')){
-            count++;
-        }
-        else{
-            count = 0;
-        }
-        if(count>0){
-            value.style.color = "green";
-        }
-        else if(count<0){
-            value.style.color = "red";
-        }
-        else{
-            value.style.color = "black";
-        }
-        value.textContent = count;
-    });
+  console.log(item);
+  item.addEventListener("click", function (e) {
+    // to select the button that has the respective class
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+  });
 });
