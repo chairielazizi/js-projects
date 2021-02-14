3ae81df7b3e1cde2ebce7e2cd8c364ba6622db43
chairielazizi <azizihr2000@gmail.com>
2021-02-14 23:41:46 +0800

    accesing the date and time components

--- Countdown/app.js
+++ Countdown/app.js
@@ -32,3 +32,22 @@ console.log(items);
 // get the current date dynamically
 let futureDate = new Date(2021, 4, 24, 11, 30, 0);
 console.log(futureDate);
+
+//accesing the time components
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+console.log(month);
+console.log(months[month]); // get the month from the months array
+month = months[month];
+
+const date = futureDate.getDate();
+
+let weekday = futureDate.getDay();
+console.log(weekday); //it is 1 because it is monday
+weekday = weekdays[futureDate.getDay()];
+
+//change the text on the index.html page
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
