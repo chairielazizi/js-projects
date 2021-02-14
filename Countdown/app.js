d3db8e5e592a3cf17b9dc94ced43850cb5750ba4
chairielazizi <azizihr2000@gmail.com>
2021-02-14 23:23:57 +0800

    get the items of time as nodelist and set the time dynamically

--- Countdown/app.js
+++ Countdown/app.js
@@ -21,3 +21,14 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadlint = document.querySelector(".deadeline");
+
+// items will return a nodelist
+const items = document.querySelectorAll(".deadline-format h4");
+console.log(items);
+
+// get the current date dynamically
+let futureDate = new Date(2021, 4, 24, 11, 30, 0);
+console.log(futureDate);
